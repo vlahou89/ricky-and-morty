ab99d28f39f0362b2e1221a3435a42e20781a8bb
Rita <rita@Ritas-MacBook-Air.local>
2023-10-17 11:39:22 +0100

    update header styles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,23 +9,23 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ searchTerm, handleSearchInput }) => {
   return (
-    <div className="sticky top-0 bg-gray-800 mb-4 z-10">
-      <div className="flex sm:flex-row sm:justify-between max-w-6xl px-2 m-auto">
+    <div className="sticky top-0 bg-gray-800 mb-4 z-10 ">
+      <div className="flex flex-col sm:flex-row sm:justify-between max-w-6xl px-2 m-auto">
         <Image
           alt="logo"
           src="https://www.freepnglogos.com/uploads/rick-and-morty-png/list-rick-and-morty-episodes-wikipedia-24.png"
           height={200}
           width={300}
-          className="flex m-auto sm:m-0 -mt-12"
+          className="flex m-auto sm:m-0"
         />
-        <div className="flex justify-center h-16 w-40 my-auto">
+        <div className="flex justify-center my-auto sm:mr-6">
           <Input
             name="query"
             type="search"
             placeholder="Search characters..."
             value={searchTerm}
             onChange={handleSearchInput}
-            className=" bg-gray-800 border border-sky-500 text-white text-sm rounded-lg focus:ring-lime-300 focus:border-lime-300 block p-2 mb-4"
+            className=" bg-gray-800 border border-sky-500 text-white text-sm rounded-lg focus:ring-lime-300 focus:border-lime-300 block"
           />
         </div>
       </div>
